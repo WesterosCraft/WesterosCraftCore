df92b7e2879d8136d9e4e5aaa5d92b5ec4608eab
jacobgranberry <jacobgranberry@gmail.com>
2025-12-17 07:39:14 -0800

    init

---
+++ src/client/java/westeroscraft/WesterosCraftCoreClient.java
@@ -0,0 +1,10 @@
+package westeroscraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WesterosCraftCoreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
